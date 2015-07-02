b8d0888e17e4f4173e491aa82b417d6f77a5de79
John Wiegley <johnw@newartisans.com>
2015-07-02 00:24:00 -0700

    Bump working version number to 0.7.0.0

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "linearscan-hoopl";
-  version = "0.6.0.0";
+  version = "0.7.0.0";
   src = ./.;
   buildDepends = [
     base containers free hoopl linearscan transformers tardis
