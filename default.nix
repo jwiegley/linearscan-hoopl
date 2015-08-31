41278cd87b07fe6474fb77081389277b03a0afbc
John Wiegley <johnw@newartisans.com>
2015-08-31 10:29:25 -0700

    Update version to 0.10.1

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "linearscan-hoopl";
-  version = "0.10.0";
+  version = "0.10.1";
   src = ./.;
   buildDepends = [
     base containers free hoopl linearscan transformers QuickCheck
